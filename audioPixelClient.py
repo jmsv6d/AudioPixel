8b420950bfc85d52494b1fa513ac17f8c0ac5e9f
Ethan Calvert <calvert1591@gmail.com>
2013-04-04 22:50:43 -0500

    Pixel id variable

--- audioPixelClient.py
+++ audioPixelClient.py
@@ -6,6 +6,7 @@ time = pygame.time
 IP = '127.0.0.1'
 PORT = 8800
 BUFFER_SIZE = 1024
+PIXEL_ID = 1
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -18,9 +19,10 @@ channel = sound.play()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, PORT))
-sock.send("1")
+sock.send(str(PIXEL_ID))
 
 while True:
 	data = sock.recv(BUFFER_SIZE)
 	s = mixer.Sound(data)
 	channel.play(s)
+
