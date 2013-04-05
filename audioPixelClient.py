848a5be6182657f949849f142462688e73490067
Ethan Calvert <calvert1591@gmail.com>
2013-04-04 22:44:45 -0500

    Add python client

---
+++ audioPixelClient.py
@@ -0,0 +1,26 @@
+import os.path, sys, socket
+import pygame.mixer, pygame.time
+mixer = pygame.mixer
+time = pygame.time
+
+IP = '127.0.0.1'
+PORT = 8800
+BUFFER_SIZE = 1024
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+file_path = os.path.join(main_dir,
+                                 'tone.wav')
+
+mixer.init(8000, 16, 1, 8192)
+sound = mixer.Sound(file_path)
+channel = sound.play()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((IP, PORT))
+sock.send("1")
+
+while True:
+	data = sock.recv(BUFFER_SIZE)
+	s = mixer.Sound(data)
+	channel.play(s)
